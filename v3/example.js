1d5dd0af64c2c9395694170c9cec2d0a9c4b11f6
yacinealioueche <alioueche.yacine@gmail.com>
2026-01-16 10:27:21 +0000

    Add files via upload

---
+++ v3/example.js
@@ -0,0 +1,49 @@
+
+import { LightningElement } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+
+// Upload these as Static Resources with these names:
+import DecompressRTF from '@salesforce/resourceUrl/DecompressRTF';
+import RTF2HTML_Lite from '@salesforce/resourceUrl/RTF2HTML_Lite';
+
+export default class CompressedRtfViewer extends LightningElement {
+  b64 = '';
+  libsLoaded = false;
+
+  renderedCallback() {
+    if (this.libsLoaded) return;
+    Promise.all([
+      loadScript(this, DecompressRTF),
+      loadScript(this, RTF2HTML_Lite)
+    ]).then(() => {
+      this.libsLoaded = true;
+    }).catch(e => console.error('Failed to load libs', e));
+  }
+
+  onInput = (e) => { this.b64 = e.target.value || ''; };
+
+  convert = () => {
+    if (!this.libsLoaded || !this.b64) return;
+
+    // base64 -> Uint8Array
+    const bin = atob(this.b64);
+    const bytes = new Uint8Array(bin.length);
+    for (let i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i) & 0xff;
+
+    try {
+      // 1) Decompress LZFu -> RTF string
+      const { rtfString } = window.DecompressRTF.decompressRTF(bytes);
+
+      // 2) RTF -> HTML (lite)
+      const html = window.RTF2HTML_Lite.rtfToHtml(rtfString);
+
+      // Inject into container
+      const el = this.template.querySelector('[data-output]');
+      el.innerHTML = html;
+    } catch (err) {
+      console.error(err);
+      const el = this.template.querySelector('[data-output]');
+      el.innerHTML = '<p class="slds-text-color_error">Failed to convert RTF.</p>';
+    }
+  };
+}
